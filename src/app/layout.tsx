b7b75304872d4c454e9c7468db3cb685d375f2fd
Sumit <sumitpadalkar53@gmail.com>
2024-07-14 11:32:57 +0530

    add: websocket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {SocketProvider} from '@/context/SocketProvider'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <SocketProvider>
+          <body className={inter.className}>{children}</body>
+        </SocketProvider>
     </html>
   );
 }
---
+++ src/context/SocketProvider.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { createContext, use, useCallback, useEffect, useMemo } from "react"
+import {io} from 'socket.io-client'
+
+interface SocketProivderProps{
+    children?:React.ReactNode
+}
+interface ISocketContext{
+    sendMessage:(msg:string)=>void,
+    sendLocation:(location:string)=>void
+}
+const SocketContext=createContext<ISocketContext|null>(null)
+
+
+export const SocketProvider:React.FC<SocketProivderProps>=({children})=>{
+
+    const sendMessage:ISocketContext['sendMessage']=useCallback((msg)=>{
+        console.log(msg)
+    },[])
+
+    useEffect(()=>{
+        const socket=io('http://localhost:8000')
+        return ()=>{
+            socket.disconnect()
+        }
+    },[])
+
+    return (
+        <SocketContext.Provider value={null}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
